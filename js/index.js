9b7fc699705d091d8a34321989143626c74a96a0
Manal Solanki <60120657+manalsolanki@users.noreply.github.com>
2020-07-20 15:46:56 -0400

    Image banner for the homepage

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// For image Slider
+
+
+var i = 0; 			
+var images = [];	
+// Time Between Switch
+var time = 3000;	
+// Image List
+images[0] = "img/banner-image1.jpg";
+images[1] = "img/banner-image2.jpg";
+
+
+// Change Image
+function SlideImg(){
+  document.getElementById('slide').src = images[i];
+  if(i < images.length - 1){
+    i++; 
+  } else { 
+    // Reset Back To O
+    i = 0;
+  }
+
+  setTimeout("SlideImg()", time);
+}
+
+// Run function when page loads
+window.onload=SlideImg;
