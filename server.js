1ce2b929bf1bf43e4793fe4c46bb69b2c1583d23
Manal Solanki <60120657+manalsolanki@users.noreply.github.com>
2020-07-24 16:42:37 -0400

    Admin sidefunctionality for the home page in progress.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express  = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
+const server = app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`)
+})
+const io = require('socket.io').listen(server)
 
 
 
@@ -10,7 +14,49 @@ app.get('/', (req, res) => {
 
 app.use(express.static('.'))
 
-// Server is running.
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`)
+const detail = (socket,data)=>{
+  io.emit('details','teamdetails')
+  socket.emit('details',{data:data})
+}
+
+io.on('connection', function (socket) 
+{
+  
+  
+  
+    socket.on('teamdetails',(teamdetails)=>{
+      console.log(teamdetails)
+    //  detail(socket,teamdetails)
+    io.emit('details',teamdetails)
+    })
+    
+
+    
+
 })
+
+
+//     socket.on('username', (data) =>{
+//       socket.broadcast.emit('username', data)
+//     })
+      
+//     // Listen for a "newuser" message
+//     socket.on('newuser', (data) => {
+//       // Transmit a message to everyone except the sender
+//       socket.broadcast.emit('newuser', data)
+  
+//       // The same message, sent to all users - try it!
+//       //io.emit('newuser', data)
+//       })
+       
+      
+//       // Listen for "chatmsg"
+//       //   io.emit to all user
+//       socket.on('chatmsg', (data) => {
+//           io.emit('chatmsg', data)
+//       })
+  
+  
+//   })
+  
+// // Server is running.
