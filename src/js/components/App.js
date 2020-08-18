3c469bc453e0de3203d6f62638efee4285705dd6
Manal Solanki <60120657+manalsolanki@users.noreply.github.com>
2020-08-18 02:37:52 -0400

    updated the conflict.

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -8,10 +8,8 @@ import {
 } from "react-router-dom"
 import Header from "components/Header"
 import Footer from "components/Footer"
-
 import NewsGrid from "components/NewsGrid"
 import NewsPage from "components/NewsPage"
-
 import MainPage from "components/MainPage"
 import Banner from "components/Banner"
 
@@ -20,22 +18,19 @@ import Banner from "components/Banner"
 const App = () => {
     return (
         <Router>
-               <Header />
-            <div>
             <Header />
-            <MainPage/>
-            <Switch >
+            <div>
+                <Header />
+                <MainPage />
+                <Switch >
 
-                <Route exact path ="/" component = {App}/>
-                <Route path ="/NewsPage" component = {NewsPage}/>
-                <Route path ="/NewsGrid" component = {NewsGrid}/>
-            </Switch>
-             
-       
-
-                <Footer />
+                    <Route exact path="/" component={App} />
+                    <Route path="/NewsPage" component={NewsPage} />
+                    <Route path="/NewsGrid" component={NewsGrid} />
+                </Switch>
+                <Footer/>
             </div>
-            
+
         </Router>
 
     )
